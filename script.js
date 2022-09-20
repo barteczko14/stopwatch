b6aac3d6b824d8a86fb678063469dc31df3e1244
Bartek <bartek.kryczka@wp.pl>
2022-09-21 00:09:52 +0200

    added a js to the project

--- script.js
+++ script.js
@@ -9,3 +9,72 @@ const timeList = document.querySelector('.time-list')
 const infoBtn = document.querySelector('.info')
 const modalShadow = document.querySelector('.modal-shadow')
 const closeModalBtn = document.querySelector('.close')
+
+let countTime;
+let seconds = 0
+let minutes = 0
+let number = 0
+
+const stop = () => {
+    clearInterval(startInterval)
+}
+
+const showInstruction = () => {
+    modalShadow.style.display = 'flex'
+}
+
+const hideInstruction = () => {
+    modalShadow.style.display ='none'
+}
+
+const handleStart = () => {
+
+    clearInterval(countTime)
+    countTime = setInterval(() => {
+        time.textContent = '';
+        seconds++
+    if ( seconds <= 9) {
+        stopwatch.textContent = `${minutes}:0${seconds}`
+    }
+    else if ( seconds >=10 && seconds <=59) {
+        stopwatch.textContent = `${minutes}:${seconds}`
+    }
+    else {
+        seconds = 0;
+        minutes++
+        stopwatch.textContent = `${minutes}:0${seconds}`
+    }
+
+    },1000);
+}
+
+const handlePause = () => {
+    clearInterval(countTime);
+}
+
+const handleStop = () => {
+    number ++
+    seconds = 0;
+    minutes = 0;
+    clearInterval(countTime);
+    time.style.visibility = "visible"
+    time.textContent = stopwatch.innerHTML;
+    timeListItem = document.createElement('li');
+    timeListItem.innerHTML = `${number} time: ${stopwatch.innerHTML}`
+    timeList.appendChild(timeListItem)
+}
+
+const clearArchive = () => {
+    stopwatch.textContent = "0:00"
+    number =0;
+    seconds = 0;
+    minutes = 0;
+    timeList.innerHTML = '';
+}
+
+infoBtn.addEventListener('click',showInstruction)
+closeModalBtn.addEventListener('click',hideInstruction)
+startBtn.addEventListener('click',handleStart)
+pauseBtn.addEventListener('click',handlePause)
+stopBtn.addEventListener('click',handleStop)
+resetBtn.addEventListener('click',clearArchive)
